288831a469e3c359535ba9031fc482826b8c1a2c
Keshav <heyitskeshav@gmail.com>
2025-06-11 01:59:09 +0530

    brian's voice automation

--- TextToSpeech/TextToSpeech_Python.py
+++ TextToSpeech/TextToSpeech_Python.py
@@ -1,1 +1,11 @@
 import pyttsx3
+def speak(text):
+    engine = pyttsx3.init()
+    voices = engine.getProperty('voices')
+    engine.setProperty('voice',voices[0].id)
+    engine.say(text)
+    engine.runAndWait()
+
+speak("hello")
+speak("hello")
+speak("hello")
